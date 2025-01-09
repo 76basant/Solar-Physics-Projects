14b089fee486f3823336ad320499d86e0d8e0bd8
Basant Magdy <91419497+76basant@users.noreply.github.com>
2025-01-09 21:55:30 +0200

    Update Root Code.py

--- Lomb Scargle Periodagram of SSA /Root Code.py
+++ Lomb Scargle Periodagram of SSA /Root Code.py
@@ -1,8 +1,12 @@
+#The objective of this Root code:
+#to use Lomb Scargle Periodogram for SSAs from Cycles 21 to 24
+
 import numpy as np
 import matplotlib.pyplot as plt
 from astropy.timeseries import LombScargle
 import pandas as pd
 from scipy.signal import find_peaks
+from scipy.interpolate import interp1d
 
 # Specify the full file path
 file_path = r'E:\Manjaro\bassant\A2\Matlab\Second Work\Lomb Scargle periodogram\file1.xlsx'
@@ -29,18 +33,18 @@ Cycle_Number='22'
 
 #select data of cycle 23
 Cycle_Number='23'
-#df=df.iloc[245:394,:]
-#start_date = pd.to_datetime('1996-08-01')
+df=df.iloc[245:394,:]
+start_date = pd.to_datetime('1996-08-01')
 
 
 #select data of cycle 24
-Cycle_Number='24'
-df=df.iloc[393:,:]
-start_date = pd.to_datetime('2008-12-01')
+#Cycle_Number='24'
+#df=df.iloc[393:,:]
+#start_date = pd.to_datetime('2008-12-01')
 
 # Calculate the absolute asymmetry
-df['Absolute_Asymmetry'] = df['SSA']
-#df['Absolute_Asymmetry'] = df['Kp_N']-df['Kp_S']
+df['Signal'] = df['SSA']
+
 
 # Ensure the 'Year' and 'Month' columns are integers
 df['Year'] = df['Year'].astype(int)
@@ -54,7 +58,7 @@ print(df)
 
 # Extract time and signal for Lomb-Scargle analysis
 time = df['Time'].values
-signal = df['Absolute_Asymmetry'].values
+signal = df['Signal'].values
 
 # Frequency range for Lomb-Scargle
 i = 1 / len(signal)  # Maximum of observed periodicities ( min of frequency)
@@ -74,11 +78,12 @@ peaks, _ = find_peaks(lombscargle, height=np.percentile(lombscargle, 95))  # Thr
 peak_frequencies = frequency[peaks]
 peak_periods = 1 / peak_frequencies
 
-#print(peaks)
-
-#print(peak_frequencies)
-
-#print(peak_periods)
+print("Peaks")
+print(peaks)
+print("peak_frequencies")
+print(peak_frequencies)
+print("peak_periods")
+print(peak_periods)
 
 for freq, period in zip(peak_frequencies, peak_periods):
     print(freq, period/12.0)
@@ -87,22 +92,20 @@ for freq, period in zip(peak_frequencies, peak_periods):
 # Confidence levels (95% and 99%)
 confidence_95 = np.percentile(lombscargle, 95)
 confidence_99 = np.percentile(lombscargle, 99)
-
+############################################
+#Plotting 
 # Creating the figure for the Lomb-Scargle periodogram
 plt.figure(figsize=(10, 6))
 
-# Plot the Lomb-Scargle periodogram
 
 #plt.plot(frequency, lombscargle, color='blue', label='Absolute Asymmetry')
 
-
-from scipy.interpolate import interp1d
 
 cubic_interploation_model = interp1d(frequency, lombscargle, kind = "cubic")
 X1_=np.linspace(0.008, 0.2, 500)
 Y2_=cubic_interploation_model(X1_)
 
-plt.scatter(frequency, lombscargle,color='blue')
+#plt.scatter(frequency, lombscargle,color='blue')
 plt.plot(X1_, Y2_,color='blue')
 
 
@@ -122,7 +125,6 @@ plt.tight_layout()
 # Get y-axis ticks
 y_ticks = plt.gca().get_yticks()
 print("Y-axis ticks:", y_ticks)
-
 # Calculate step size (assuming at least two ticks)
 y_step_size = y_ticks[1] - y_ticks[0]
 print("Step size of y-axis:", y_step_size)
@@ -131,7 +133,8 @@ print("Step size of y-axis:", y_step_size)
 for freq, period in zip(peak_frequencies, peak_periods):
     print(freq, period/12.0)
     plt.axvline(x=freq, color='green', linestyle=':', linewidth=1)
-    #plt.text(freq, np.max(lombscargle)-y_step_size/3, f'{period/12.0:.2f} yr', color='red',horizontalalignment='right', verticalalignment='center', fontsize=12, rotation=90)
+    plt.text(freq, np.max(lombscargle), f'{period/12.0:.2f} yr', color='red',
+             horizontalalignment='right', verticalalignment='center', fontsize=12, rotation=90)
 
     
     
